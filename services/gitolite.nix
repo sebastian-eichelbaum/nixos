3454530adf90f765b95e0eaf5e873c4bed33b4ee
Sebastian Eichelbaum <code@nemtics.com>
2024-07-12 20:10:33 +0200

    [AUTO] auto-commit 2024-07-12T20:10:33+0200

--- services/gitolite.nix
+++ services/gitolite.nix
@@ -6,6 +6,7 @@
   #
   # A simple git repository server. Its config is stored in a git repo itself.
   #
+  # git clone git@HOSTNAME:gitolite-admin
 
   services.gitolite = {
     enable = true;
@@ -25,6 +26,8 @@
   };
 
   # The doc states: it is our own obligation to create the dataDir if it is not the default one.
-  systemd.tmpfiles.rules =
-    [ "d ${config.services.gitolite.dataDir} 0770 git git -" ];
+  systemd.tmpfiles.rules = [
+    # ATTENTION: ssh does not like group writeable home dirs!
+    "d ${config.services.gitolite.dataDir} 0750 git git -"
+  ];
 }
