aabecb9462ea8152b44f740bcd805589d24b8c30
Sebastian Eichelbaum <code@nemtics.com>
2025-02-15 23:46:40 +0100

    Gamemode changes

--- programs/gaming.nix
+++ programs/gaming.nix
@@ -17,6 +17,11 @@
 
     # VR
     # sidequest
+
+    # Utils
+
+    # Prevents screensaver/xset dpms when gamepads are used
+    joystickwake
   ];
 
   #############################################################################
@@ -24,10 +29,26 @@
   #
 
   # Unlike its name, it is not only useful for games.
+  # Attention: add the group "gamemode" to your user.
   programs.gamemode.enable = true;
   programs.gamemode.enableRenice = true;
   programs.gamemode.settings = {
-    general = { renice = 10; };
+    general = {
+      # Negated and used as nice value
+      renice = 0;
+      desiredgov = "performance";
+      # Several games profit from disabling splitlock
+      disable_splitlock = 1;
+
+      # Should prevent the screensaver but does not seem to work for xset blackening and dpms
+      inhibit_screensaver = 1;
+    };
+
+    cpu = {
+      park_cores = true;
+      pin_cores = true;
+    };
+
     # NOTE: those scripts can also be set in ~/.config/gamemode.ini
     custom = {
       start = ''
